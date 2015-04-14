674c7932ca865a5d6304a3a205080d97314894ca
prudhvid <dharmana.prudhvi@gmail.com>
2015-04-14 22:23:49 +0530

    added files for data update

---
+++ github/code/get_csv.py
@@ -0,0 +1,113 @@
+__author__ = '12CS10037'
+__author__ = '12CS10037'
+import  networkx as nx;
+import graphlab as gl
+
+import sqlite3
+import sys
+
+conn=sqlite3.connect('../data/sqlite/projects.sql.sqlite3.db')
+
+cursor=conn.cursor()
+
+g=nx.Graph();
+
+g=nx.read_pajek("./watchers.net")
+print 'got watchers'
+proj_members=nx.read_pajek('./project_members.net')
+print 'got data'
+# print proj_members.nodes()
+users_proj_dic={}
+users=[]
+projects=[]
+rating=[]
+
+for node in g.nodes():
+    if node[0]=='u':
+        p=g.neighbors(node)
+
+        for tp in p:
+            users_proj_dic[(node,tp)]=5
+
+
+for node in proj_members.nodes():
+    if node[0] == 'u':
+        p = proj_members.neighbors(node)
+
+        for tp in p:
+            key=(node,tp)
+            users_proj_dic[key] = users_proj_dic.get(key, 0) + 11
+            cursor.execute("select id,forked_from from projects where id=%s"%(tp[1:]))
+            res=cursor.fetchone()
+            if not res or not res[1]:
+                continue
+
+            key=(node,'p'+str(res[1]))
+            users_proj_dic[key]=users_proj_dic.get(key, 0) + 7
+
+
+for key,value in users_proj_dic.items():
+    users.append(key[0])
+    projects.append((key[1]))
+    rating.append(value)
+
+print users[0:100]
+print projects[0:100]
+print rating[0:100]
+print len(rating),len(projects),len(users)
+data=gl.SFrame({
+    'user_id':users,
+    'item_id':projects,
+    'rating':rating
+    })
+
+uprojects=list(set(projects))
+upnames=[]
+uforked=[]
+ulanguage=[]
+udescription=[]
+
+rlist=[]
+
+for p in uprojects:
+    cursor.execute("select owner_id,name,language,forked_from,description from projects where id=%s"%(p[1:]))
+    # print "select owner_id,name,language,forked_from from projects where id=%s"%(p[1:])
+    r=cursor.fetchone()
+    if not r:
+        rlist.append(p)
+        continue
+    # print r
+    upnames.append(r[1])
+    ulanguage.append(r[2])
+    if r[3]:
+        uforked.append(1)
+    else:
+        uforked.append(0)
+    udescription.append(r[4])
+
+for r in rlist:
+    uprojects.remove(r)
+print len(uprojects),len(upnames),len(uforked),len(ulanguage)
+
+itemdata=gl.SFrame({
+    'item_id':uprojects,
+    'name':upnames,
+    'language':ulanguage,
+    'forked_from':uforked,
+    'description':udescription
+})
+
+print itemdata
+itemdata.save('csv/project_data',format='binary')
+data.save('csv/data',format='binary')
+# model=gl.recommender.factorization_recommender.create(data,target='rating',item_data=itemdata)
+# print model
+# gl.load_sframe()
+# model.save('models/final_large')
+# results=model.recommend(k=5)
+# # print results
+# results.save('results_final.csv',format='csv')
+
+
+
+
--- github/code/test.py
+++ github/code/test.py
@@ -22,22 +22,53 @@ pcursor=conn2.cursor()
 
 # sf = graphlab.SFrame({'user_id': ["0", "0", "0", "1", "1", "2", "2", "2"],'item_id': ["a", "b", "c", "a", "b", "b", "c", "d"],'rating': [1, 3, 2, 5, 4, 1, 4, 3]})
 # m = graphlab.recommender.create(sf, target='rating')
-model=gl.load_model('models/final')
-print 'loaded model',time()
-data=gl.SFrame({
-    'user_id':['5'],
-    # 'item_id':['10','20']
-    })
+# model=gl.load_model('models/watcher_item')
+
+# print 'loaded model',time()
+
+sf=gl.load_sframe('csv/data')
+pdata=gl.load_sframe('csv/project_data')
+pdata=gl.SFrame(
+    {
+        'item_id':pdata['item_id'],
+        'language':pdata['language'],
+        'name':pdata['name'],
+        'description':pdata['description']
+    }
+)
+# print sf['user_id']
+model=gl.recommender.ranking_factorization_recommender.create(sf,target='rating',item_data=pdata)
+model.save('models/pdata_max')
+# model=gl.load_model('models/rfm_rating')
+
+
 
 while True:
-    u=raw_input("enter username")
-    cursor.execute("select * from users where login='%s'"%(u))
-    user=cursor.fetchone()
-    print user
-    results=model.recommend(k=5,users=[str(user[0])],verbose=True)
-    print results
-    for p in results['item_id']:
-        pcursor.execute("select * from projects where id = %s"%(p[1:]))
-        print pcursor.fetchall()
-# recs = m.recommend()
-# recs.save('recs');
+    try:
+        u=raw_input("enter username")
+        cursor.execute("select * from users where login='%s'"%(u))
+        user=cursor.fetchone()
+        # print user[0]
+        # for row in sf:
+        #     count=0;
+        #     if row['user_id']=='u'+str(user[0]):
+        #         print row
+        #         count=count+1
+        #         if count<11:
+        #             break;
+
+
+        print user
+        if not user:
+            print 'no username'
+            continue
+        results=model.recommend(k=5,users=['u'+str(user[0])],verbose=True)
+        print results
+        for p in results['item_id']:
+            pcursor.execute("select * from projects where id = %s"%(p[1:]))
+            x=pcursor.fetchone()
+            # print x
+            ans=str(x[1]).replace('api.', '').replace('repos/','')
+            print ans
+    except:
+        pass
